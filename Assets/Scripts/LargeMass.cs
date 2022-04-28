b6e6d4fc4bfe12a206ee8da2232cde934f6a448f
ghkkhan <huzaifakhan3339@gmail.com>
2022-04-27 21:13:54 -0500

    Added basic script to the planets
    Rotation

---
+++ Assets/Scripts/LargeMass.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeMass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+{
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(new Vector3(0f, 0.1f, 0f));   
+    }
+}
