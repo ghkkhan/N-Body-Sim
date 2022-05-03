dd0d51dcc00018418140ff68bfbb4e0e9bd2858c
ghkkhan <huzaifakhan3339@gmail.com>
2022-05-03 16:21:55 -0500

    Added Gradual Time manipulation
    Also added back the third planet!

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ButtonBehavior : MonoBehaviour {
+    private const float PLAY = 1f;
+    private const float PAUSED = 0f;
+    private const float SPEDUP = 2f;
+    private const float SLOWDOWN = 0.5f;
+
+    private float a = PLAY;
+
     public void OnPauseButtonPress() {
-        Time.timeScale = 0;
+        a = PAUSED;
     }
     public void OnPlayButtonPress() {
-        Time.timeScale = 1;
+        a = PLAY;
     }
     public void OnTwoXSpeed() {
-        Time.timeScale = 2;
+        a = SPEDUP;
     }
     public void onHalfSpeed() {
-        Time.timeScale = 0.5f;
+        a = SLOWDOWN;
+    }
+
+    void TimeStep(float target) {
+        if (Time.timeScale > target) Time.timeScale -= 0.04f;        
+        else Time.timeScale += 0.04f;
+        if (Mathf.Abs(target - Time.timeScale) < 0.05f) Time.timeScale = target;
+    }
+    void Update() {
+        if (Time.timeScale != a) TimeStep(a);
     }
 }
