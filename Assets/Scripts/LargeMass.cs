872908b9843bf5addcef2f48575c2b11de6e1d0a
Zachary S <zasample18+github@gmail.com>
2022-05-03 21:45:17 -0500

    Velocity arrows (Correct in all dimensions).

--- Assets/Scripts/LargeMass.cs
+++ Assets/Scripts/LargeMass.cs
@@ -16,15 +16,20 @@ public class LargeMass : MonoBehaviour {
 
     // FixedUpdate is called once per "tick"
     void FixedUpdate() {
+        // Rotate the earth (visual effect)
         transform.Rotate(new Vector3(0f, 10f, 0f) * Time.deltaTime);
-        velocityArrow.localScale = new Vector3(0.1f, velocity.magnitude, 0.1f);
 
-        float zAngle = Mathf.Rad2Deg * Mathf.Atan2(velocity.y, velocity.x) + 90;
+        // Scale the velocity arrow to the magnitude of the velocity (plus a bit less than the radius of the world)
+        velocityArrow.localScale = new Vector3(0.1f, velocity.magnitude + 0.9f, 0.1f);
 
-        Debug.Log(velocity + "" + zAngle);
-
-        velocityArrow.rotation = Quaternion.Euler(0.0f, 0.0f, zAngle);
-
+        // Reposition the arrow to the current position of the world.
         velocityArrow.position = transform.position + velocity;
+        
+        // Rotate the velocity arrow to the direction of velocity.
+        // LookAt takes a world position, so we calculate the position to look at
+        velocityArrow.LookAt(velocityArrow.position + velocity);
+        // LookAt actually makes the forward axis point at the position, so we rotate 90 degrees
+        // around the x axis to have the up axis point at the position.
+        velocityArrow.Rotate(new Vector3(90, 0, 0));
     }
 }
