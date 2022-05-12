cb7b32bf3ebcd27a3788cbf1aab821f6dfbc76f2
Zachary S <zasample18+github@gmail.com>
2022-05-12 13:20:19 -0500

    Add user-controlled camera zoom.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
     public Transform follow;
+    public float zoomDistance = 30f;
 
     // Update is called once per frame
     void Update() {
-        transform.position = follow.position - new Vector3(0f, 0f, 30f);
+        // Scroll mouse to zoom camera in or out
+        zoomDistance -= Input.mouseScrollDelta.y;
+
+        // Position camera based on center of mass
+        transform.position = follow.position - Vector3.forward * zoomDistance;
     }
 }
