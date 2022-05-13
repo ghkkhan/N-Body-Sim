1df74aa56a626273de6316ec4b9ad8a5e0a362c7
Zachary S <zasample18+github@gmail.com>
2022-05-12 21:53:11 -0500

    Add different planet textures and add time speed indicator.

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -1,36 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class ButtonBehavior : MonoBehaviour {
-    private const float PLAY = 1f;
-    private const float PAUSED = 0f;
-    private const float SPEDUP = 2f;
-    private const float SLOWDOWN = 0.5f;
-
-    public float goalSpeed = PLAY;
+    public float goalSpeed = 1.0f;
+    public TMP_Text speedText;
 
     public void OnPauseButtonPress() {
-        goalSpeed = PAUSED;
+        goalSpeed = 0.0f;
     }
     public void OnPlayButtonPress() {
-        goalSpeed = PLAY;
+        goalSpeed = 1.0f;
     }
     public void OnTwoXSpeed() {
-        goalSpeed = SPEDUP;
+        if (goalSpeed > 0.0f) {
+            goalSpeed *= 2.0f;
+        } else {
+            goalSpeed = 2.0f;
+        }
     }
     public void onHalfSpeed() {
-        goalSpeed = SLOWDOWN;
+        if (goalSpeed > 0.0f) {
+            goalSpeed *= 0.5f;
+        } else {
+            goalSpeed = 0.5f;
+        }
+    }
+    public void onMenu() {
+        SceneManager.LoadScene("Menu");
     }
 
     // Gradually change timeScale when a button is pressed, for a smoother experience
     void TimeStep(float target) {
-        if (Time.timeScale > target) Time.timeScale -= 0.04f;        
-        else Time.timeScale += 0.04f;
         if (Mathf.Abs(target - Time.timeScale) < 0.05f) Time.timeScale = target;
+        else if (Time.timeScale == 0.0f) Time.timeScale = 0.1f;
+        else if (Time.timeScale > target && Time.timeScale < target + 1.0f) Time.timeScale -= 0.04f;
+        else if (Time.timeScale < target && Time.timeScale > target + 1.0f) Time.timeScale += 0.04f;
+        else if (Time.timeScale > target) Time.timeScale *= 0.9f;
+        else if (Time.timeScale < target) Time.timeScale *= 1.1f;
     }
+
+    // Apply gradual change, and show current speed in GUI.
     void Update() {
         if (Time.timeScale != goalSpeed) TimeStep(goalSpeed);
+        if (speedText != null) {
+            speedText.text = "Current speed: " + Time.timeScale;
+        }
     }
 }
